bfcfee68163ec58a089e2f62cb6f610666339937
Paul Murray <plmrry@gmail.com>
2022-07-30 16:05:38 +0000

    basic server

---
+++ app.js
@@ -0,0 +1,13 @@
+import http from "node:http"
+
+const port = process.env.PORT ?? 8080
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    data: 'Hello World!'
+  }));
+});
+
+server.listen(port);
